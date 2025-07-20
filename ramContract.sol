fbfc124e5c2515b0bc85c0f37b787e37c87a5862
Coronel4545 <stifler.oliveira41@gmail.com>
2025-07-20 20:00:37 -0300

    Update ramContract.sol

--- ramContract.sol
+++ ramContract.sol
@@ -132,7 +132,7 @@ abstract contract nonReentrant {
 }
 
 /**
- * @title RAM_CEO_TOKEN
+ * @title RAM_TECH_TOKEN
  * @notice Main token contract implementing BEP20/ERC20 with advanced features.
  */
 contract RAM_TECH_TOKEN is Context, IBEP20, Ownable, nonReentrant {
